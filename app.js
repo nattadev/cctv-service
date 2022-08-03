c06cb67e3bb8b7dce9b944cd4566eb53da3f18e6
ื <>
2022-08-03 11:52:56 +0700

    update type query

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const db = require("./config/database");
+const { ApolloServer, gql } = require("apollo-server-express");
+const typeDefs = require('./typeDefs')
+const resolvers = require('./resolvers')
 
-db.authenticate()
-  .then(() => console.log("database connected"))
-  .catch((err) => console.log("Eror : " + err));
 
-const app = express();
+async function startServer() {
+  db.authenticate()
+    .then(() => console.log("database connected"))
+    .catch((err) => console.log("Eror : " + err));
 
-const helloRouter = require("./route/user");
+  const app = express();
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
 
-app.use("/", helloRouter);
+  await apolloServer.start();
 
-app.listen(5050, () => console.log("Server started in port", 5050));
+   apolloServer.applyMiddleware({ app:app , path: '/users'})
+
+   app.use((req, res) => {
+    res.json('hello')
+   } )
+
+  app.listen(5050, () => console.log("Server started in port", 5050));
+}
+
+startServer();
--- route/user.js
+++ route/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controller/userController");
 
-router.get("/", userController.index);
+
+router.get("/user", userController.index);
 
 module.exports = router;
